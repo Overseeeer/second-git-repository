0c0a332c18bd6457ea141ea814b6bad6cf6844ea
Overseeeer <gabriel_e_fagundes@estudante.sesisenai.org.br>
2025-04-09 21:43:17 -0300

    feat: Adicionando uma feature nova, o Scanner!

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,8 +1,16 @@
+import java.util.Scanner;
 class HelloWorld{
 
     public static void main(String [] args){
 
+        Scanner scan = new Scanner(System.in);
+
         System.out.println("Hello, World! :)");
+
+        System.out.printf("\nDigite o seu nome: ");
+        String nome = scan.nextLine();
+
+        System.out.println("\nSeu nome é: " + nome);
 
     }
 
